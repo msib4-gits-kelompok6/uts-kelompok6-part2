7d7a04188fec9f16917aa0194a8ff939c3c8cab3
Kuro-Maul <gm1322878@gmail.com>
2023-04-11 17:28:20 +0800

    Tambah auth login register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
@@ -19,24 +20,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
+// auth
+
+Route::get('/login', [AuthController::class, "login"])->name('login');
+Route::get('/register', [AuthController::class, "register"])->name('register');
+Route::get('/logout', [AuthController::class, "logout"])->name('logout');
+
+Route::post('/register', [AuthController::class, "doRegister"])->name('do.register');
+Route::post('/login', [AuthController::class, "dologin"])->name('do.login');
+
+
+
 // category
-Route::prefix('category')->group(function () {
-    Route::get('/', [CategoryController::class, 'index']);
-    Route::get('/add', [CategoryController::class, 'create']);
-    Route::get('/{id}/edit', [CategoryController::class, 'edit']);
-    Route::get('/{id}/delete', [CategoryController::class, 'destroy']);
-    Route::post('/', [CategoryController::class, 'store']);
-    Route::put('/{id}', [CategoryController::class, 'update']);
+Route::middleware(['auth:web'])->group(function () {
+    Route::prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']);
+        Route::get('/add', [CategoryController::class, 'create']);
+        Route::get('/{id}/edit', [CategoryController::class, 'edit']);
+        Route::get('/{id}/delete', [CategoryController::class, 'destroy']);
+        Route::post('/', [CategoryController::class, 'store']);
+        Route::put('/{id}', [CategoryController::class, 'update']);
+    });
 });
 
+
+
 // product
-Route::prefix('product')->group(function () {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::get('/add', [ProductController::class, 'create']);
-    Route::get('/{id}/edit', [ProductController::class, 'edit']);
-    Route::get('/{id}/delete', [ProductController::class, 'destroy']);
-    Route::post('/', [ProductController::class, 'store']);
-    Route::put('/{id}', [ProductController::class, 'update']);
+Route::middleware(['auth:web'])->group(function () {
+    Route::prefix('product')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/add', [ProductController::class, 'create']);
+        Route::get('/{id}/edit', [ProductController::class, 'edit']);
+        Route::get('/{id}/delete', [ProductController::class, 'destroy']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+    });
 });
 
 // cart
